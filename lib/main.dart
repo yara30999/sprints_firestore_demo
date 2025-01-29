c8227ae4d93499efaf235257b07640b67a36c1d3
yara30999 <yaranaserelniny@gmail.com>
2025-01-29 18:00:47 +0200

    setup firebase to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:sprints_firestore_demo/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
