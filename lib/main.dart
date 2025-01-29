b6fcb979a56c250b2a48f0a9ce4d1e0b4716082d
yara30999 <yaranaserelniny@gmail.com>
2025-01-29 18:11:36 +0200

    navigate to home empty home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sprints_firestore_demo/firebase_options.dart';
+import 'package:sprints_firestore_demo/screens/home_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -16,6 +18,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Firestore Demo',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
